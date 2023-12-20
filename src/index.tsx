b1b55ae66a5dbc3f8596bc0c90fa39648fb91369
MarcinMG-web <gierada.marcin@gmail.com>
2023-12-20 13:48:59 +0100

    added index.css file

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 import 'bootstrap/scss/bootstrap.scss';
 import { AppProvider } from './GlobalState/AppState';
