3ad9ca54b32b52f1d826c7acd5a2f3ca1b81432d
MarcinMG-web <gierada.marcin@gmail.com>
2023-12-20 11:02:06 +0100

    remove unusefull wrapper

--- src/FormWrapper/Field.tsx
+++ src/FormWrapper/Field.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, ReactElement } from 'react';
+import React, { ReactNode } from 'react';
 import { FieldError, Merge, FieldErrorsImpl } from 'react-hook-form';
 
 type FieldProps = {
@@ -8,30 +8,11 @@ type FieldProps = {
 };
 
 export function Field({ children, label, error }: FieldProps): JSX.Element {
-  const id = getChildId(children);
-
   return (
     <div className='col-sm-12 mb-3'>
-      <label htmlFor={id} className='form-label'>
-        {label}
-      </label>
+      <label className='form-label'>{label}</label>
       {children}
       {error && <small className='error'>{error?.message as string}</small>}
     </div>
   );
 }
-
-type ChildProps = {
-  id?: string;
-};
-
-// Get id prop from a child element
-export const getChildId = <T extends ChildProps>(
-  children: ReactNode
-): string | undefined => {
-  const child = React.Children.only(children) as ReactElement<T>;
-
-  if (child && 'id' in child.props) {
-    return child.props.id;
-  }
-};
